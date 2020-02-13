9650c5503cd56ed7c3d61e7d6ce205de6971a55e
jayshreethakare <54076809+jayshreethakare@users.noreply.github.com>
2020-02-13 11:25:02 +0530

    Add files via upload

---
+++ formatting.py
@@ -0,0 +1,28 @@
+def unorganized(a, b): 
+    for i in range (a, b): 
+        print ( i, i**2, i**3, i**4 ) 
+  
+# Function prints the organized set of values 
+def organized(a, b): 
+    for i in range (a, b): 
+  
+        # Using formatters to give 6  
+        # spaces to each set of values 
+        print("{:6d} {:6d} {:6d} {:6d}"
+        .format(i, i ** 2, i ** 3, i ** 4)) 
+  
+# Driver Code 
+n1 = int(input("Enter lower range :-\n")) 
+n2 = int(input("Enter upper range :-\n")) 
+  
+print("------Before Using Formatters-------") 
+  
+# Calling function without formatters 
+unorganized(n1, n2) 
+print() 
+print("-------After Using Formatters---------") 
+print() 
+  
+# Calling function that contain 
+# formatters to organize the data 
+organized(n1, n2) 
